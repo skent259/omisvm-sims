dc6ca24dc02d11371b048c6a4d94ef6a56859411
spkent <spkent@submit3.chtc.wisc.edu>
2022-06-06 09:13:52 -0500

    minor updates

--- condor/chtc/combine.sh
+++ condor/chtc/combine.sh
@@ -5,6 +5,7 @@ tar -xzf packages.tar.gz
 export PATH=$PWD/R/bin:$PATH
 export RHOME=$PWD/R
 export R_LIBS=$PWD/packages
+
 tar -xzf output.tar.gz 
-Rscript output/combine_files.R output/per-tma per-tma-2.0.0 1 output/per-tma
+Rscript output/combine_files.R output/per-tma per-tma-2.0.0 2 output/per-tma
 tar -czf output.tar.gz output/
--- output/combine_files.sh
+++ output/combine_files.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 2.0.0
+Rscript combine_files.R output/per-tma/ per-tma-2.0.0 1 output/per-tma
+Rscript combine_files.R output/per-tma/ per-tma-2.0.0 2 output/per-tma
+
 # 6.0.0
 Rscript combine_files.R /z/Comp/spkent/simulation/omisvm-sims/6.0/ wr-car-6.0.0 1 output/wr-car
 cp wr-car/omisvm-sims-results-wr-car-6.0.0-1.rds /z/Comp/spkent/simulation/omisvm-sims/6.0
