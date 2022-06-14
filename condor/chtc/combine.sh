0b113b8d9721da0cc09b9125594f41ce44e22420
spkent <spkent@submit3.chtc.wisc.edu>
2022-06-14 10:44:41 -0500

    add size-wq step 2 files

--- condor/chtc/combine.sh
+++ condor/chtc/combine.sh
@@ -7,5 +7,8 @@ export RHOME=$PWD/R
 export R_LIBS=$PWD/packages
 
 tar -xzf output.tar.gz 
-Rscript output/combine_files.R output/size-swd size-swd-4.0.0 1 output/size-swd
+Rscript output/combine_files.R output/size-wq size-wq-5.0.0 1d output/size-wq
 tar -czf output.tar.gz output/
+
+rm packages.tar.gz 
+rm R412.tar.gz 
--- condor/chtc/unzip.sh
+++ condor/chtc/unzip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Unzips the directories returned from condor
-name="size-swd-4.0.0-1"
+name="size-wq-5.0.0-1d"
 find . -name "sim-$name*.tar.gz" -exec tar -xzf {} \;
